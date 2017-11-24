6fce9ad47ebadf05e12a500ead918c3683b93527
navashameed@gmail.com <navashameed@gmail.com>
2017-11-24 10:34:14 +0530

    one more change

--- app/src/main/java/donate/tcs/com/myapplication/activities/ActionsListActivity.java
+++ app/src/main/java/donate/tcs/com/myapplication/activities/ActionsListActivity.java
@@ -3,16 +3,23 @@ package donate.tcs.com.myapplication.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import donate.tcs.com.myapplication.R;
 
-public class MainActivity extends AppCompatActivity {
+public class ActionsListActivity extends BaseActivity {
+
+    private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFirebaseAuth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -26,5 +33,30 @@ public class MainActivity extends AppCompatActivity {
 
     public void onAddEntryClicked(View v){
         startActivity(new Intent(this, AddEntryActivity.class));
+    }
+
+    private void loadLogInView() {
+        Intent intent = new Intent(this, LogInActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_logout) {
+            mFirebaseAuth.signOut();
+            loadLogInView();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
